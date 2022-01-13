c3475d716b0b5070ae9f7b35688d675dcd5424ac
Maciej Kaźmierczyk <67171102+macie-kk@users.noreply.github.com>
2022-01-13 23:23:47 +0100

    :rocket: Dodano strukture klasy osoby i podstawowe okno

---
+++ src/person/PersonColors.java
@@ -0,0 +1,10 @@
+package person;
+
+import java.awt.Color;
+
+public interface PersonColors {
+	public static final Color INFECTED = new Color(217, 67, 60);
+	public static final Color DOCTOR = new Color(122, 173, 245);
+	public static final Color VAXXED = new Color(153, 245, 122);
+	public static final Color NORMAL = new Color(184, 184, 184);
+}
