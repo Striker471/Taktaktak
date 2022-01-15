9e62d4ff05be50c7a697d48874cc0335894ec8e0
camonday <arkadiakowalczyk@gmail.com>
2022-01-15 14:45:08 +0100

    Poprawa dostępności na rozszerzanie - klasa Person nie potrezbuje klasy Window bo granice podawane są do konstruktora

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,7 +1,23 @@
 package person;
 
+/*
+ * AKowalczyk 15.01.2022: 	Moim zdaniem lepiej przekazać wymiary window.HEIGHT, WIDTH. Gdyby ktoś chciał
+ * 							wykorzystać klasę Person ale nie koniecznie z klasą window lub tym bardziej
+ * 							pakietem app. Można podawać je przy tworzeniu obiektu ale ustawić żeby tylko
+ * 							przy pierwszym odpaleniu konstruktora się zapisywały.
+ *
+ * 							Alternatywnie można też przy pierwszym odpaleniu konstruktora zapytać klase
+ * 							wywołującą konstruktor o podanie wymiarów jeśli któś wie jak to zrobić
+ *
+ * Zmiany: 	> zakomentowanie linijek "import static app.Window.HEIGHT" i analogicznej dla WIDTH
+ * 			> dodanie zmiennych statycznych WIDTH i HEIGHT oraz ich inicjalizacji w konstruktorach
+ * 			> konstruktor z podaną pozycją wywołuje konstruktora z losową a nie na odwrót
+ */
+
+/*
 import static app.Window.HEIGHT;
 import static app.Window.WIDTH;
+*/
 
 import java.util.Random;
 
@@ -9,6 +25,11 @@ public abstract class Person {	// musi rozszerzac jakas klase od ksztaltu
 	
 	private static final int MARGIN = 10;	// margines spawnowania
 	private static final Random RND = new Random();
+
+	private static int HEIGHT;
+	private static int WIDTH;
+	private static boolean czyIstnieje = false;
+
 
 	private double x;	// obecny X
 	private double y;	// obecny Y
@@ -19,25 +40,36 @@ public abstract class Person {	// musi rozszerzac jakas klase od ksztaltu
 	private final boolean doctor;		// informacja czy jest doktorem
 	
 	/* konstruktor z losowa pozycja */
-	public Person(boolean doctor) {
-		this(RND.nextInt(WIDTH-2*MARGIN) + MARGIN, RND.nextInt(HEIGHT-2*MARGIN) + MARGIN, doctor);
-	}
-	
-	/* pelny konstruktor */
-	public Person(double x, double y, boolean doctor) {
+	public Person(boolean doctor, int w, int h) {
 		this.doctor = doctor;
-		this.x = x;
-		this.y = y;
-		
+
+		if(!czyIstnieje){
+			WIDTH=w;
+			HEIGHT=h;
+			czyIstnieje=true;
+		}
+
+		x=RND.nextInt(WIDTH - 2 * MARGIN) + MARGIN;
+		y=RND.nextInt(HEIGHT - 2 * MARGIN) + MARGIN;
+
 		/* poczatkowy wektor: + albo - (przy kolizji z krawedzia okna bedzie zmienial znak) */
 		this.vx = RND.nextBoolean() ? 4 : -4;
 		this.vy = RND.nextBoolean() ? 4 : -4;
-		
+
 		/*
 		 * Ustawienie x/y i koloru w metodach Swinga
 		 */
+
 	}
+
 	
+	/* Konstruktor z podaną pozycją */
+	public Person(double x, double y, boolean doctor, int w, int h) {
+		this(doctor, w, h);
+		this.x = x;
+		this.y = y;
+	}
+
 	public void setInfected(boolean val) {
 		this.infected = val;
 		// zmiana koloru metoda Swinga
