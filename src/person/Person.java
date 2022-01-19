8e84833fd1ca9d9d87d5d1dd3353657f172bd95e
Woleek <d.woleek@gmai.com>
2022-01-19 14:05:53 +0100

    Fixed pull #3

--- src/person/Person.java
+++ src/person/Person.java
@@ -5,27 +5,32 @@ import java.util.Random;
 
 import static app.Window.HEIGHT;
 import static app.Window.WIDTH;
+import static java.lang.Math.abs;
 
-public class Person extends Rectangle {	// musi rozszerzac jakas klase od ksztaltu
+public class Person extends Rectangle {    // musi rozszerzac jakas klase od ksztaltu
 	private static final int size = 4;
-	private static final int MARGIN = 10;	// margines spawnowania
+	private static final int MARGIN = 1;    // margines spawnowania
 	private static final Random RND = new Random();
 
 
-	private double x;	// obecny X
-	private double y;	// obecny Y
-	private double vx;	// wektor X
-	private double vy;	// wektor Y
-	private boolean infected = false;	// informacja czy jest zarazony
-	private boolean vaxxed = false;		// informacja czy jest zaszczepiony
-	private final boolean doctor;		// informacja czy jest doktorem
+	private double x;    // obecny X
+	private double y;    // obecny Y
+
+
+	private static final double maxiV = 8.0;
+	private static final double miniV = 4.0;
+	private double vx;    // wektor X
+	private double vy;    // wektor Y
+	private boolean infected = false;    // informacja czy jest zarazony
+	private boolean vaxxed = false;        // informacja czy jest zaszczepiony
+	private final boolean doctor;        // informacja czy jest doktorem
 	private Color color;
-	
+
 	/* konstruktor z losowa pozycja */
 	public Person(boolean doctor) {
-		this(RND.nextInt(WIDTH-2*MARGIN) + MARGIN, RND.nextInt(HEIGHT-2*MARGIN) + MARGIN, doctor);
+		this(RND.nextInt(WIDTH - 2 * MARGIN) + MARGIN, RND.nextInt(HEIGHT - 2 * MARGIN) + MARGIN, doctor);
 	}
-	
+
 	/* pelny konstruktor */
 	public Person(double x, double y, boolean doctor) {
 		this.doctor = doctor;
@@ -35,50 +40,92 @@ public class Person extends Rectangle {	// musi rozszerzac jakas klase od ksztal
 		/* poczatkowy wektor: + albo - (przy kolizji z krawedzia okna bedzie zmienial znak) */
 		this.vx = RND.nextBoolean() ? 4 : -4;
 		this.vy = RND.nextBoolean() ? 4 : -4;
-		color = doctor ? PersonColors.DOCTOR: PersonColors.NORMAL;
+		color = doctor ? PersonColors.DOCTOR : PersonColors.NORMAL;
 
 		/*
 		 * Ustawienie x/y i koloru w metodach Swinga
 		 */
 	}
-	
+
 	public void setInfected(boolean val) {
 		this.infected = val;
 		this.color = PersonColors.INFECTED;
 		// zmiana koloru metoda Swinga
 	}
-	
+
 	public void setVaxxed(boolean val) {
 		this.vaxxed = val;
 		this.color = PersonColors.VAXXED;
 		// zmiana koloru metoda Swinga
 	}
-		
+
 	public boolean isInfected() {
 		return infected;
 	}
-	
+
 	public boolean isVaxxed() {
 		return vaxxed;
 	}
-	
+
 	public boolean isDoctor() {
 		return doctor;
 	}
-	
+
 	public double getX() {
 		return x;
 	}
-	
+
 	public double getY() {
 		return y;
 	}
-	
+
+	public double getVx() {
+		return vx;
+	}
+
+	public double getVy() {
+		return vy;
+	}
+
+	private boolean setX(double newbieX) {
+		if (newbieX > 0.0 && newbieX < WIDTH) {
+			x = newbieX;
+			return true;
+		} else if (newbieX <= 0.0) {
+			x = 0.0;
+		} else {
+			x = WIDTH;
+		}
+		return false;
+	}
+
+	private boolean setY(double newbieY) {
+		if (newbieY > 0.0 && newbieY < HEIGHT) {
+			y = newbieY;
+			return true;
+		} else if (newbieY <= 0.0) {
+			y = 0.0;
+		} else {
+			y = HEIGHT;
+		}
+		return false;
+	}
+
 	public void move() {
-		/*
-		 * 	setX(getX() + vx);
-		 * 	setY(getY() + vy);
-		 * 	+ wykrywanie kolizji z oknem
-		 */
+
+		if (!(setX(getX() + vx))) {
+			vx = -vx / abs(vx);
+			vx = vx * (RND.nextDouble() * (maxiV - miniV) + miniV);
+			//vx = 4 * RND.nextDouble();
+			// vx *= RND.nextBoolean() ? 1 : -1;
+		}
+		if (!(setY(getY() + vy))) {
+			vy = -vy / abs(vy);
+			vy = vy * (RND.nextDouble() * (maxiV - miniV) + miniV);
+		}
+
+
+		// 	+ wykrywanie sąsiadów
+
 	}
 }
